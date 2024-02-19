ef04e06b36c3db3cab1ee32d3829340c55d08c47
cang8 <0.gizemgunes@gmail.com>
2024-02-19 20:20:44 +0300

    added-business-services

---
+++ KodlamaIo/Business/Abstract/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using KodlamaIo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KodlamaIo.Business.Abstract
+{
+    public interface ICategoryService : IGenericService<Category>
+    {
+    }
+}
---
+++ KodlamaIo/Business/Abstract/IGenericService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KodlamaIo.Business.Abstract
+{
+    public interface IGenericService<T> where T : class
+    {
+        void Add(T entity);
+        T GetById(int id);
+        List<T> GetAll();
+        void Update(T entity);
+        void Delete(int id);
+    }
+}
