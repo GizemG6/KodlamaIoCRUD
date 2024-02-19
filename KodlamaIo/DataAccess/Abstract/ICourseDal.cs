396a7ab46e51b9d72e099a88c06d96c6aa8f9064
cang8 <0.gizemgunes@gmail.com>
2024-02-19 18:56:38 +0300

    added-dataaccess-abstract

--- KodlamaIo/DataAccess/Abstract/ICourseDal.cs
+++ KodlamaIo/DataAccess/Abstract/ICourseDal.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KodlamaIo.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace KodlamaIo.DataAccess.Abstract
 {
-    public interface ICourseDal
+    public interface ICourseDal : IGenericDal<Course>
     {
     }
 }
--- KodlamaIo/DataAccess/Abstract/IGenericDal.cs
+++ KodlamaIo/DataAccess/Abstract/IGenericDal.cs
@@ -8,6 +8,10 @@ namespace KodlamaIo.DataAccess.Abstract
 {
     public interface IGenericDal<T> where T : class
     {
-       
+        void Add(T entity);
+        T GetById(int id);
+        List<T> GetAll();
+        void Update(T entity);
+        void Delete(T entity);
     }
 }
